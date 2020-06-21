6502c82d2ed522651308fd838738ba8d164f6255
Feng Guo <fengsterooni@yahoo.com>
2020-06-20 18:06:46 -0700

    Added OpenCV native libraries.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,27 @@
 #include <jni.h>
-#include <string>
+#include <android/log.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_nativeopencvkotlin_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#define TAG "NativeLib"
+
+using namespace std;
+using namespace cv;
+
+extern "C" {
+void JNICALL Java_com_example_nativeopencvkotlin_MainActivity_adaptiveThresholdFromJNI(JNIEnv *env,
+                                                                                       jobject instance,
+                                                                                       jlong imgAddr) {
+
+    Mat &img = *(Mat *) imgAddr;
+
+    clock_t begin = clock();
+
+    cv::adaptiveThreshold(img, img, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 21, 5);
+
+    double total_time = double(clock() - begin) / CLOCKS_PER_SEC;
+
+    __android_log_print(ANDROID_LOG_INFO, TAG, "adaptiveThreshold computation time = %f seconds\n",
+                        total_time);
+}
 }
--- app/src/main/java/com/example/nativeopencvkotlin/MainActivity.kt
+++ app/src/main/java/com/example/nativeopencvkotlin/MainActivity.kt
@@ -1,29 +1,124 @@
 package com.example.nativeopencvkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
+import android.view.SurfaceView
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
+import org.opencv.core.Mat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity(), CameraBridgeViewBase.CvCameraViewListener2 {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+    private val mLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(TAG, "OpenCV loaded successfully")
+
+                    // Load native library after(!) OpenCV initialization
+                    System.loadLibrary("native-lib")
+
+                    mOpenCvCameraView!!.enableView()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "called onCreate")
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(
+
+            this@MainActivity,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST
+        )
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(R.layout.activity_main)
+
+        mOpenCvCameraView = findViewById<CameraBridgeViewBase>(R.id.main_surface)
+
+        mOpenCvCameraView!!.visibility = SurfaceView.VISIBLE
+
+        mOpenCvCameraView!!.setCvCameraViewListener(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mOpenCvCameraView!!.setCameraPermissionGranted()
+                } else {
+                    val message = "Camera permission was not granted"
+                    Log.e(TAG, message)
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+            }
+            else -> {
+                Log.e(TAG, "Unexpected permission request")
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback)
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!")
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {}
+
+    override fun onCameraViewStopped() {}
+
+    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
+        val img = inputFrame.gray()
+        adaptiveThresholdFromJNI(img.nativeObjAddr)
+        return img
+    }
+
+    private external fun adaptiveThresholdFromJNI(imgGray: Long)
 
     companion object {
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+
+        private const val TAG = "com.example.nativeopencvkotlin.MainActivity"
+        private const val CAMERA_PERMISSION_REQUEST = 1
     }
 }
